7281d09e673403f43ae98dc687a81f11b7b4bdcd
Pushkin <respect4me@ukr.net>
2021-01-29 22:49:39 +0200

    big update

--- script1.js
+++ script1.js
@@ -4,6 +4,10 @@ let series = document.getElementById("series");
 let episode = document.getElementById("episode");
 let result_div = document.getElementById('result_div');
 let pagination_div = document.getElementById('pagination');
+let modal_description = document.getElementById('modal_description');
+
+let description_div = document.getElementById('description_div');
+
 
 
 button.onclick = function () {
@@ -41,6 +45,7 @@ button.onclick = function () {
 
     })
     .then(newObject => {
+
       if (newObject.hasOwnProperty('Error')) {
         let single_div = document.createElement('div');
         single_div.className = 'single_div';
@@ -50,17 +55,45 @@ button.onclick = function () {
       for (key in newObject.Search) {
         let single_div = document.createElement('div');
         single_div.className = 'single_div';
-        single_div.innerHTML = `<div class="back"><h1>${newObject.Search[key].Title}</h1><h2>${newObject.Search[key].Year}</h2></div><img src='${newObject.Search[key].Poster}' alt='poster'></img>`;
+        single_div.innerHTML = `<div class="back" id='${newObject.Search[key].imdbID}'><h1>${newObject.Search[key].Title}</h1><h2>${newObject.Search[key].Year}</h2><button class="modal_btn">more...</button></div><img src='${newObject.Search[key].Poster}' alt='poster'></img>`;
         if (single_div.lastElementChild.getAttribute('src') === 'N/A') {
           single_div.lastElementChild.setAttribute('src', './images/no_poster.jfif');
         }
         result_div.append(single_div);
+        // descriptoin modal
         single_div.onclick = (e) => {
-          let x = e.target.closest('div');
-          x.classList.toggle('zzz');
+
+          if (e.target.className == "modal_btn") {
+            modal_description.style.display = 'block';
+            modal_description.onclick = () => {
+              modal_description.style.display = 'none';
+            }
+            while (description_div.firstChild) {
+              description_div.removeChild(description_div.firstChild);
+            }
+            const { id } = e.target.closest('div');
+            let description_url = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=9af90fb8`;
+            fetch(description_url)
+              .then(response => {
+                return response.json()
+              })
+              .then(description_info => {
+                const { Title, Actors, Awards, BoxOffice, Country, Director, Genre, Plot, imdbRating, Website } = description_info;
+                let content_div = document.createElement('div');
+                content_div.className = "draw_info";
+                content_div.innerHTML = `<h2>${Title}</h2><p>Actors: ${Actors}</p><p>Awards: ${Awards}</p><p>${BoxOffice}</p><p>${Country}</p><p>Director:${Director}</p><p>${Genre}</p><p>${Plot}</p><p>ImdbRating:${imdbRating}</p>`;
+                description_div.append(content_div)
+                console.log(description_info);
+              })
+
+          }
+
 
         }
+
       }
+
+
       // pagination
       let all_pages = Math.ceil(newObject.totalResults / 10);
 
@@ -85,15 +118,41 @@ button.onclick = function () {
               for (key in newObject.Search) {
                 let single_div = document.createElement('div');
                 single_div.className = 'single_div';
-                single_div.innerHTML = `<div class="back"><h1>${newObject.Search[key].Title}</h1><h2>${newObject.Search[key].Year}</h2></div><img src='${newObject.Search[key].Poster}' alt='poster'></img>`;
+                single_div.innerHTML = `<div class="back" id='${newObject.Search[key].imdbID}'><h1>${newObject.Search[key].Title}</h1><h2>${newObject.Search[key].Year}</h2><button class="modal_btn">more...</button></div><img src='${newObject.Search[key].Poster}' alt='poster'></img>`;
                 if (single_div.lastElementChild.getAttribute('src') === 'N/A') {
                   single_div.lastElementChild.setAttribute('src', './images/no_poster.jfif')
                 }
                 result_div.append(single_div);
 
+                ///click on poster(after paginationClick)
+
                 single_div.onclick = (e) => {
-                  let x = e.target.closest('div');
-                  x.classList.toggle('zzz');
+
+                  if (e.target.className == "modal_btn") {
+                    modal_description.style.display = 'block';
+                    modal_description.onclick = () => {
+                      modal_description.style.display = 'none';
+                    }
+                    while (description_div.firstChild) {
+                      description_div.removeChild(description_div.firstChild);
+                    }
+                    const { id } = e.target.closest('div');
+                    let description_url = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=9af90fb8`;
+                    fetch(description_url)
+                      .then(response => {
+                        return response.json()
+                      })
+                      .then(description_info => {
+                        const { Title, Actors, Awards, BoxOffice, Country, Director, Genre, Plot, imdbRating, Website } = description_info;
+                        let content_div = document.createElement('div');
+                        content_div.className = "draw_info";
+                        content_div.innerHTML = `<h2>${Title}</h2><p>Actors: ${Actors}</p><p>Awards: ${Awards}</p><p>${BoxOffice}</p><p>${Country}</p><p>Director:${Director}</p><p>${Genre}</p><p>${Plot}</p><p>ImdbRating:${imdbRating}</p>`;
+                        description_div.append(content_div)
+                        console.log(description_info);
+                      })
+
+                  }
+
 
                 }
               }
